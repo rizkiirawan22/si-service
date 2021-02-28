e8b58daaec696975b6e705e5bfc7835eabd3cc13
Rizki Irawan <rizkiirawan22998@gmail.com>
2021-03-01 05:35:44 +0700

    added view tambah service

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 if (empty($_SESSION['username']) and empty($_SESSION['passwd'])) {
     header('location:login/index.php');
 } else {
-    ?>
+?>
     <!DOCTYPE html>
     <html>
 
@@ -69,113 +69,108 @@ if (empty($_SESSION['username']) and empty($_SESSION['passwd'])) {
     </head>
 
     <body class="hold-transition sidebar-mini layout-fixed">
-    <div class="wrapper">
+        <div class="wrapper">
 
-        <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-            <!-- Left navbar links -->
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                </li>
-            </ul>
-        </nav>
-        <!-- /.navbar -->
-        <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <!-- Brand Logo -->
-            <a href="index.php" class="brand-link">
-                <img src="assets/img/logo.jpg" alt="Si-Service" class="brand-image img-circle elevation-3"
-                     style="opacity: .8">
-                <span class="brand-text"><b>SI</b>Service</span>
-            </a>
+            <!-- Navbar -->
+            <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+                <!-- Left navbar links -->
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- /.navbar -->
+            <!-- Main Sidebar Container -->
+            <aside class="main-sidebar sidebar-dark-primary elevation-4">
+                <!-- Brand Logo -->
+                <a href="index.php" class="brand-link">
+                    <img src="assets/img/logo.jpg" alt="Si-Service" class="brand-image img-circle elevation-3" style="opacity: .8">
+                    <span class="brand-text"><b>SI</b>Service</span>
+                </a>
 
-            <!-- Sidebar -->
-            <div class="sidebar">
-                <!-- Sidebar user panel (optional) -->
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="assets/img/guest-user.jpg" class="img-circle elevation-2" alt="User Image">
+                <!-- Sidebar -->
+                <div class="sidebar">
+                    <!-- Sidebar user panel (optional) -->
+                    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                        <div class="image">
+                            <img src="assets/img/guest-user.jpg" class="img-circle elevation-2" alt="User Image">
+                        </div>
+                        <div class="info">
+                            <span class="d-block text-light"><?= $_SESSION['nama']; ?></span>
+                        </div>
                     </div>
-                    <div class="info">
-                        <span class="d-block text-light"><?= $_SESSION['nama']; ?></span>
-                    </div>
-                </div>
 
-                <!-- Sidebar Menu -->
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                        data-accordion="false">
-                        <!-- Add icons to the links using the .nav-icon class
+                    <!-- Sidebar Menu -->
+                    <nav class="mt-2">
+                        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                            <!-- Add icons to the links using the .nav-icon class
                     with font-awesome or any other icon font library -->
-                        <li class="nav-item">
-                            <a href="index.php?page=dashboard"
-                               class="nav-link <?= ($_GET['page'] == 'dashboard') ? 'active' : ''; ?>">
-                                <i class="nav-icon fas fa-tachometer-alt"></i>
-                                <p>
-                                    Dashboard
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="index.php?page=service"
-                               class="nav-link  <?= ($_GET['page'] == 'service') ? 'active' : ''; ?>">
-                                <i class="nav-icon fas fa-cogs"></i>
-                                <p>
-                                    Data Service
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="index.php?page=laporan"
-                               class="nav-link <?= ($_GET['page'] == 'laporan') ? 'active' : ''; ?>">
-                                <i class="nav-icon fas fa-file-alt"></i>
-                                <p>
-                                    Laporan
-                                </p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="app/logout.php" class="nav-link">
-                                <i class="nav-icon fas fa-sign-out-alt"></i>
-                                <p>
-                                    Logout
-                                </p>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <!-- /.sidebar-menu -->
+                            <li class="nav-item">
+                                <a href="index.php?page=dashboard" class="nav-link <?= ($_GET['page'] == 'dashboard') ? 'active' : ''; ?>">
+                                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                                    <p>
+                                        Dashboard
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="index.php?page=service" class="nav-link  <?= ($_GET['page'] == 'service') ? 'active' : ''; ?>">
+                                    <i class="nav-icon fas fa-cogs"></i>
+                                    <p>
+                                        Data Service
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="index.php?page=laporan" class="nav-link <?= ($_GET['page'] == 'laporan') ? 'active' : ''; ?>">
+                                    <i class="nav-icon fas fa-file-alt"></i>
+                                    <p>
+                                        Laporan
+                                    </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="app/logout.php" class="nav-link">
+                                    <i class="nav-icon fas fa-sign-out-alt"></i>
+                                    <p>
+                                        Logout
+                                    </p>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <!-- /.sidebar-menu -->
+                </div>
+                <!-- /.sidebar -->
+            </aside>
+
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <?php include "app/config.php"; ?>
             </div>
-            <!-- /.sidebar -->
-        </aside>
+            <!-- /.content-wrapper -->
+            <footer class="main-footer">
+                <strong>Copyright &copy; 2021 <b>SI</b>Service</strong>
+                All rights reserved.
+            </footer>
 
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <?php include "app/config.php"; ?>
+            <!-- Control Sidebar -->
+            <aside class="control-sidebar control-sidebar-dark">
+                <!-- Control sidebar content goes here -->
+            </aside>
+            <!-- /.control-sidebar -->
         </div>
-        <!-- /.content-wrapper -->
-        <footer class="main-footer">
-            <strong>Copyright &copy; 2021 <b>SI</b>Service</strong>
-            All rights reserved.
-        </footer>
+        <!-- ./wrapper -->
 
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
-    </div>
-    <!-- ./wrapper -->
-
-    <!-- jQuery -->
-    <script src="assets/js/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="assets/js/adminlte.min.js"></script>
+        <!-- jQuery -->
+        <script src="assets/js/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="assets/js/adminlte.min.js"></script>
 
     </html>
-    <?php
+<?php
 }
 ?>
