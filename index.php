7e47a3e059f7f426744db50f02ae970a6802e94f
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-02-28 20:01:15 +0700

    edited title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>si-Service | Dashboard</title>
+    <title>SIService</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
@@ -31,9 +31,9 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
+            <a href="index.php" class="brand-link">
                 <img src="assets/img/logo.jpg" alt="Si-Service" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">si-Service</span>
+                <b>SI</b><span class="brand-text font-weight-light">Service</span>
             </a>
 
             <!-- Sidebar -->
