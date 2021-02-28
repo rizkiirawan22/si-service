24c618c1ce8aeb9b8a933dc873068b2eb7cf439a
Rizki Irawan <rizkiirawan22998@gmail.com>
2021-02-28 20:09:58 +0700

    added connection

--- app/connection.php
+++ app/connection.php
@@ -1,1 +1,19 @@
 <?php
+
+$host = "localhost";
+$username = "root";
+$pass = "";
+$db = "si_service_dwirizki";
+
+$conn = mysqli_connect($host, $username, $pass, $db);
+
+function query($query)
+{
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
