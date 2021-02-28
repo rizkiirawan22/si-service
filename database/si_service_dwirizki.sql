3f66f64c12e7241a06f8b0f76510f1fecbdfa88b
Rizki Irawan <rizkiirawan22998@gmail.com>
2021-02-28 20:01:16 +0700

    new db

--- database/si_service_dwirizki.sql
+++ database/si_service_dwirizki.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2021 at 01:36 PM
+-- Generation Time: Feb 28, 2021 at 01:59 PM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
