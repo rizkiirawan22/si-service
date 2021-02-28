363cbe57c363893467637fdfff7e6a7b883fe5c8
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-02-28 20:19:04 +0700

    fix logo SIService

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
             <!-- Brand Logo -->
             <a href="index.php" class="brand-link">
                 <img src="assets/img/logo.jpg" alt="Si-Service" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <b>SI</b><span class="brand-text font-weight-light">Service</span>
+                <span class="brand-text"><b>SI</b>Service</span>
             </a>
 
             <!-- Sidebar -->
