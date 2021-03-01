22bebd85ba246a59c737e6a26010535a26f36e68
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-03-01 10:25:37 +0700

    small changes

--- services/create.php
+++ services/create.php
@@ -3,12 +3,12 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0 text-dark">Tambah Service</h1>
+                <h1 class="m-0 text-dark">Add Service Data</h1>
             </div><!-- /.col -->
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Data Services</a></li>
-                    <li class="breadcrumb-item active">Tambah</li>
+                    <li class="breadcrumb-item"><a href="services/?page=1">Data Services</a></li>
+                    <li class="breadcrumb-item active">Create</li>
                 </ol>
             </div><!-- /.col -->
         </div><!-- /.row -->
